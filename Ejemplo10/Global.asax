d057f2f698fe34662306dade07a750c11ac6540b
Gabriel Alminana <r470n@yahoo.com.ar>
2016-11-07 16:54:10 -0300

    Ejercicios 8-9-10

---
+++ Ejemplo10/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ejemplo10.MvcApplication" Language="C#" %>
---
+++ Ejemplo10/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ejemplo10.Startup))]
+namespace Ejemplo10
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
